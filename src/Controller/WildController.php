7c844bff3bf96a50ace8114253d30841aef02d54
Raphonx <r.moulet@live.fr>
2020-05-25 11:02:42 +0200

    Le routing avancé

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,14 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild")
+ */
+
 Class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route ("/", name="wild_index")
+     * @return Response
      */
+
     public function index() : Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries']);
+    }
+
+    /**
+     * @Route("/show/{slug}", requirements={"slug" = "^[a-z\d\-]+$"}, name="wild_show")
+     */
+    public function show(string $slug) : Response
+    {
+        $slug = ucwords($slug, "-");
+        $slug = preg_replace("/-/", " ", $slug);
+        return $this->render('wild/show.html.twig', ['slug' => $slug]);
     }
 }
